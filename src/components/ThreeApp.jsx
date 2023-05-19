0caf5100b556ee3821d6d1df179f50a733ce6f8e
DRT Software <tony@drt-software.com>
2023-05-19 01:18:18 +0100

    Add leva gui

--- src/components/ThreeApp.jsx
+++ src/components/ThreeApp.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { OrbitControls, Box, Stage } from "@react-three/drei";
 import { Lucy } from "../models/Lucy.jsx";
+import { useControls } from "leva";
 
 const ThreeApp = () => {
+  const { name, aNumber } = useControls({ name: "World", aNumber: 0 });
   return (
     <>
       <Stage
